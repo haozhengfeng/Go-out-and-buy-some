b85c83088b2adc79d3f3cf5ae0aba0a6ffcb781d
haozhengfeng <haozhengfeng123@163.com>
2018-02-12 09:37:10 +0800

    调整窗口大小

--- buysome-parent/buysome-back/src/main/webapp/js/main.js
+++ buysome-parent/buysome-back/src/main/webapp/js/main.js
@@ -1,8 +1,7 @@
 $(function(){
+	
 	$("#mainFrame").attr('src', $(".active a[href-url]").attr("href-url"));
 	
-	$("#mainFrame").css('min-height', (parseInt($(".content-wrapper").css('min-height'))-16)+"px");
-
 	$(".sidebar-menu li").each(function(){
 		if($(this).find(".treeview-menu").length>0){
 			$(this).find(".treeview-menu").find("a").click(function(){
@@ -14,4 +13,10 @@ $(function(){
 			});
 		}
 	});
+	
+	//alert($(".content-wrapper").height());
+	//alert($(".content-wrapper").css('min-height'));
+	
+	//$("#mainFrame").css('min-height', (parseInt($(".content-wrapper").css('min-height'))-16)+"px");
+	$("#mainFrame").css('min-height', ($(".content-wrapper").height()-16)+"px");
 });
